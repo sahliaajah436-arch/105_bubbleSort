688418ffc546842065945f5888e2531d856d2c75
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-05 11:34:46 +0700

    menambahkan display

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -49,3 +49,22 @@ void bubbleSortArray(){
 
     } while (pass <= n - 1);
 }
+
+void display() {
+    cout << endl;
+    cout << "==================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "==================" << endl;
+    cout << endl;
+    for (int j = 0; j  <= n; j++) {
+        cout << arr[j];
+        if ( j < n - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
