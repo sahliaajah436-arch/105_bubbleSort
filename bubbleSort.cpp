7974bebfb1c6dcddeab809e9e387525a7667ce1c
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-05 11:43:44 +0700

    menambahkan return

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -57,14 +57,25 @@ void display() {
     cout << "==================" << endl;
     cout << endl;
     for (int j = 0; j  <= n; j++) {
-        cout << arr[j];
+        cout << arr[j]; // menampilkan array
         if ( j < n - 1) {
             cout << " --> ";
         }
     }
     cout << endl;
     cout << endl;
-    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << "Jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
     cout << endl;
     cout << endl;
+}
+
+int main() {
+    
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
 }
